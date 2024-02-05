2d2e6047f2c2962911d9829eea4e8061816ea5e1
DevGame <t.seagames@hotmail.com>
2024-02-05 09:41:54 +0700

    fix cant change project name in eng form

--- src/service/apiCreatePDF.js
+++ src/service/apiCreatePDF.js
@@ -346,21 +346,12 @@ function run(excel, form) {
             fontSize: 24,
             absolutePosition: { x: 50, y: 250 + valueMargin },
           }
-        : form.language === "Eng" &&
-          (form.pj_code.substring(0, 3) === "PAR" ||
-            form.pj_code.substring(0, 4) === "ASST")
-        ? {
-            text: "Student Science Training Program" + " " + form.currentYear,
+        : {
+            text: form.pj_name,
             color: "#1565C0",
             fontSize: 28,
             absolutePosition: { x: 50, y: 300 + valueMargin },
             bold: true,
-          }
-        : {
-            text: "“" + form.pj_name + "”",
-            color: "#1565C0",
-            fontSize: 24,
-            absolutePosition: { x: 50, y: 280 + valueMargin },
           },
 
       // (form.language === "TH") ? { text: "“" + form.pj_name + "”", color: '#1565C0', fontSize: 24 } : {},
